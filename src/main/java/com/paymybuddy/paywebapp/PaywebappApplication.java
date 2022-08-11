80861079e6d0faf04c922dc979668b2d2839600c
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2022-08-11 19:16:25 +0200

    Initial commit

---
+++ src/main/java/com/paymybuddy/paywebapp/PaywebappApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaywebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/paywebapp/PaywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
